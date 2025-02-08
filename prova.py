b03ed46fc7f79cd9f84f61cea3e3bff03f4e5e68
Andrey Rafael <andrey.raf130@gmail.com>
2025-02-08 09:00:41 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,4 @@
+print("forneça as informações abaixo: ")
+baseTri = int(input("Digite a base do triangulo: "))
+altura = int(input("Digite a altura do triangulo: "))
+print(f"Area = {baseTri * altura}")
