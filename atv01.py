b8bb8f3beb2dca55ee1b76e0f19f02e20c793054
Andrey Rafael <andrey.raf130@gmail.com>
2025-02-08 08:59:58 -0300

    Add files via upload

---
+++ atv01.py
@@ -0,0 +1,7 @@
+#ATIVIDADE
+nome = "Andrey"
+print("Defina as sequintes informações abaixo")
+paisUser = str(input("Digite seu pais: "))
+populationPais = float(input("Digite a população do pais onde voce mora")) #216.4
+pib = float(input("digite o PIB do pais onde voce mora: ")) #2.174
+print(f"prazer {nome}, voce nasceu no {paisUser} onde a população é de {populationPais}, e o PIB é de {pib}")
